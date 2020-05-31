f3c83a68be67b192afe57cefb254e16b6fc97953
ashish <asisbhudeopd1995@gmail.com>
2020-05-31 22:36:23 +0530

    added populate database function

---
+++ README.md
@@ -0,0 +1,35 @@
+**Version 1.0.0**
+
+A readme for an API for getting data of Activity Tracker of a user.
+---
+
+## Contributers 
+
+-Ashish Kumar <ashis08h@gmail.com> 
+
+---
+
+## Technologies
+
+	Python 3.8.2
+	Django 3.0.5
+	
+---
+## Library
+
+	Faker - To populate database with fake data
+	djano restframework
+	sphinx - For documentation the project.
+---
+
+## Launch
+
+ pythonanywhere - http://ashis08h.pythonanywhere.com/ (project)
+
+ GitHub - https://github.com/ashis08h/fullthrottel (project)
+
+ pythonanywhere - (documentation)
+
+## License & copyright
+
+FullThrottle Labs.
--- user_app/views.py
+++ user_app/views.py
@@ -2,6 +2,7 @@ from user_app.models import UserDetails,ActivityPeriod
 from user_app.serializers import UserDetailsSerializers,ActivityPeriodSerializers
 from rest_framework import viewsets
 from django.http import HttpResponse
+from faker import Faker
 
 
 
@@ -9,37 +10,21 @@ class UserViewSet(viewsets.ModelViewSet):
     queryset = UserDetails.objects.all()
     serializer_class = UserDetailsSerializers
 
+
 def population(request):
-	import os
-	os.environ.setdefault("DJANGO_SETTINGS_MODULE",'user_api.settings')
-	import django
-	django.setup()
-	import random
-	from user_app.models import UserDetails,ActivityPeriod
-	from faker import Faker
 	fakegen=Faker()
-	user_person=['Egon Spengler', 'Glinda Southgood','Joshua Mason'] 
-	def add_user_person():
-		t=UserDetails.objects.get_or_create(real_name=random.choice(user_person))[0]
-		t.save()
-		return t
-	def populate(N=5):
-		for entry in range(N):
-			user_per=add_user_person()
-			fake_country=fakegen.timezone()
-			fake_start_time=fakegen.date_time()
-			fake_end_time=fakegen.date_time()
-			if len(UserDetails.objects.filter(real_name = user_per)) == 1 :
-				print(UserDetails.objects.filter(real_name = user_per))
-				print(len(UserDetails.objects.filter(real_name = user_per)))
-				print(user_per)
-				popuser=UserDetails.objects.get_or_create(real_name=user_per)[0]
-				popactivity=ActivityPeriod.objects.get_or_create(user_id=popuser,start_time=fake_start_time, end_time=fake_end_time)[0]
-			else:
-				user_per=add_user_person()
-				popuser=UserDetails.objects.get_or_create(real_name=user_per,tz=fake_country)[0]
-				popactivity=ActivityPeriod.objects.get_or_create(user_id=popuser,start_time=fake_start_time, end_time=fake_end_time)[0]
-			
-	populate(3)
+	user_person=['Egon Spengler', 'Glinda Southgood','Joshua Mason','Ashish Kumar'] 
+	for item in user_person:
+		fake_start_time=fakegen.date_time()
+		fake_end_time=fakegen.date_time()
+		fake_country=fakegen.timezone()
+		try:
+			user_details=UserDetails.objects.get(real_name = item)
+
+		except UserDetails.DoesNotExist:
+			user_details=UserDetails(real_name = item,tz = fake_country)
+			user_details.save()
+		popactivity=ActivityPeriod(user_id=user_details,start_time=fake_start_time, end_time=fake_end_time)
+		popactivity.save()
 	return HttpResponse("your database is populated with dumy data.")
 
